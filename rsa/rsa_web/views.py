912ed4f6815faee32158133dc66e5a2ac049dc94
aPostelnicu <67789319+aPostelnicu@users.noreply.github.com>
2020-12-31 16:10:14 +0200

    Frontend

--- rsa/rsa_web/views.py
+++ rsa/rsa_web/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Message
+from .forms import MessageForm
 
-# Create your views here.
+def main(request):
+    form = MessageForm()
+
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {
+        'form': form
+    }
+    return render(request, "rsa_web/index.html", context)
